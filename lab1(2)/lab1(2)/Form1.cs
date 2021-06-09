df0da197deabbad61f57d927a78070c5500b76d9
Dzavialov <58293091+Dzavialov@users.noreply.github.com>
2021-06-09 15:25:15 +0300

    Add files via upload

---
+++ lab1(2)/lab1(2)/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_2_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control x in Controls)
+            {
+                if (x is Label)
+                {
+                    x.Text = "";
+                }
+            }
+        }
+    }
+}
---
+++ lab1(4)/lab1(4)/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_4_
+{
+    public partial class Form1 : Form
+    {
+        int cnt = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label label = new Label();
+            label.Text = "Label №" + cnt.ToString();
+            label.Font = new Font("Arial", 10, FontStyle.Bold);
+            label.Location = new Point(15, 30 * cnt);
+            label.Size = new Size(800, 20);
+            this.Controls.Add(label);
+            cnt++;
+        }
+    }
+}
